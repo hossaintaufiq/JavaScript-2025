fab2486fd8a6378d8eca27a83cab2ff8cd3dbb97
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-01-07 12:48:16 +0600

    chapter 1 done

--- hello.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello world')
-console.log('this is our js code ')
-
-for(let i=0;i<5;i++){
-    console.log(i+' ')
-}
---
+++ variables & datatype.js
@@ -0,0 +1,43 @@
+// console.log('Hello world')
+// console.log('this is our js code ')
+
+// for(let i=0;i<5;i++){
+//     console.log(i+' ')
+// }
+
+// name = "Hossain Ahmmed "
+// name= "25"
+// console.log(name);
+// age = 22
+// console.log(age)
+
+// console = "hello world" 
+// console.log(console); 
+// should use camelCase 
+// let price = 25 ; 
+// let money = 450000; 
+// let fullName = "Hossain"
+
+const student = { 
+    age: 24, 
+    fullName: "Hossain Ahmmed ", 
+    versity: "North South University ", 
+    id: 2232102042
+}; 
+
+//  we can print the object info in two diffent formats \
+//  object.key or object[key]
+
+// console.log(student.age)
+// console.log(student["versity"])
+
+const product = {
+    fullName:" Parker Jotter Standard CT Ball Pen ", 
+    color: "Black", 
+    rating: "4.5", 
+    price: 270,
+    isDeal: true
+}
+
+
+// chapter 1 done 
