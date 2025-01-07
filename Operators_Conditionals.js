9ed1b7d4ec566178d147d06a1efe98cf7466b487
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-01-07 13:18:18 +0600

    Chapter 2

---
+++ Operators_Conditionals.js
@@ -0,0 +1,9 @@
+console.log("Chapter 2 (Operators and conditionals)")
+
+// commnets are written in two steps // or /* .. */ 
+// exponential multiplication a**b = a^ b; 
+
+let a= 5, b=2; 
+
+let c = a**b; 
+console.log (c) ; // the answer will be 25 
