23ef43c9d2fe81f426c4ff30af3b98b5735fd99d
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-01-08 16:25:31 +0600

    array

--- Operators_Conditionals.js
+++ Operators_Conditionals.js
@@ -1,9 +1,24 @@
-console.log("Chapter 2 (Operators and conditionals)")
+// console.log("Chapter 2 (Operators and conditionals)")
 
 // commnets are written in two steps // or /* .. */ 
 // exponential multiplication a**b = a^ b; 
 
-let a= 5, b=2; 
+// let a= 5, b=2; 
 
-let c = a**b; 
-console.log (c) ; // the answer will be 25 
+// let c = a**b; 
+// console.log (c) ; // the answer will be 25 
+
+
+
+//  conditionals 
+
+// let num=20;
+// let num=prompt("Enter a number")
+
+// if (num%2==0){
+//     console.log(num, "is even")
+
+// }
+// else {
+//     console.log(num, "is odd")
+// }
---
+++ loops and Strings.js
@@ -0,0 +1,39 @@
+console.log("Chapter 3: loops and Strings ")
+
+// for(let i = 0; i <5;i++){
+//     console.log(i+" ->Chapter 3 ")
+// }
+
+// do while looop 
+
+// let i=2 ;
+
+// do{
+//     console.log(i + " -->>Chapter 3")
+//     i++;
+// }while(i<=10)
+
+
+// for of loop 
+
+// let str = "Hosain Ahmmed"
+// let size=0;
+// for(let i of str)
+// {
+//     console.log(i)
+//     size++;
+// }
+// console.log(size)
+
+// for in loop 
+
+let student2 = {
+    name: "Hossain Ahmmed ",
+    age: 18,
+    cgpa:3.33, 
+    isPass: true,
+}
+
+for( let key in student2 ){
+    console.log(key, "= ", student2[key])
+}
