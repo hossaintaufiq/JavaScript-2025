cfe9d97460a8366ce20a946fcae54f6ddb54e6bb
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-01-13 17:07:39 +0600

    functions and methods

--- array.js
+++ array.js
@@ -1,3 +1,25 @@
-console.log("Lecture 4 : array ")
+// console.log("Lecture 4 : array ")
 
 
+// let marks = [98, 96,94,94,64]
+
+// console.log(marks.length)
+// console.log(marks)
+
+// // push and pop method  for an array 
+
+// marks.push(100);
+// console.log(marks)
+
+// marks.pop()
+// console.log(marks)
+
+// // to string method for array 
+
+// marks.toString()
+// console.log(marks)//it returns the array in a string version 
+
+// typeof(marks)
+
+
+// splice , slice method 
---
+++ funciton.js
@@ -0,0 +1,31 @@
+// console.log("functions in js")
+
+// // normal function
+
+// let str= prompt("Enter your String");
+
+// // let str= "Hello"
+
+// let countVowel=(str)=>{
+//     str.toLowerCase()
+//     let count =0;
+//     for(let i of str){
+//         if(i == 'a' || i == 'e' ||i== 'i'||i== 'o' ||i=='u'){
+//             count++;
+//     }
+
+//  }
+//  return count ;
+// }
+
+// console.log(`There are ${countVowel(str)} vowels in the str`)
+
+// arrow functions
+
+// // let a=5,b=9;
+// let  arrowmalti= (a,b) =>{
+//     return a*b;
+// }
+
+// // arrowmalti(4,5)
+// console.log(arrowmalti(3,4))
